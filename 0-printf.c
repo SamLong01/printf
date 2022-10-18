4a55c2e4d384ace2997103f19148af45158210bd
Adebisola <adeniyiadebisola@gmail.com>
2022-10-18 05:13:32 -0700

    testing

--- 0-printf.c
+++ 0-printf.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _printf - writes an output into the standard output
+ * _printf - prints and input into the standard output
  * @format: the format string
  * Return: number of bytes printed
  */
 
 int _printf(const char *format, ...)
+
 {
 	int sum = 0;
 	va_list ap;
@@ -16,21 +17,24 @@ int _printf(const char *format, ...)
 
 	va_start(ap, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
+	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		if (*p != '%')
+		init_params(&params, ap);
+		if (*p != '%')/*checking for the % specifier*/
 		{
 			sum += _putchar(*p);
 			continue;
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params))
-			p++;
+		while (get_flag(p, &params)) /* while char at p is flag character */
+		{
+			p++; /* next character */
+		}
 		p = get_width(p, &params, ap);
 		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
